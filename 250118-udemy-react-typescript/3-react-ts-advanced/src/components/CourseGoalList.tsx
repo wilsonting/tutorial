359f0df972ceb27d9f931e475404fa897bc28aaa
diduboyz <wlsn_ting520@hotmail.com>
2025-01-19 20:31:37 +0800

    add CourseGoalList component

--- 250118-udemy-react-typescript/3-react-ts-advanced/src/components/CourseGoalList.tsx
+++ 250118-udemy-react-typescript/3-react-ts-advanced/src/components/CourseGoalList.tsx
@@ -1,4 +1,6 @@
+import { type ReactNode } from "react";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 export type CourseGoalModel = {
   title: string;
@@ -15,15 +17,37 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  //dynamically render the component using if condition
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goal yet. Start to add one!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
