23db6c21cd8be46271bead107a7ad60de31d4d96
diduboyz <wlsn_ting520@hotmail.com>
2025-02-04 21:13:37 +0800

    add useState and dependencies

--- 250118-udemy-react-typescript/6-react-ts-side-effect-data-fetch/src/components/Timer.tsx
+++ 250118-udemy-react-typescript/6-react-ts-side-effect-data-fetch/src/components/Timer.tsx
@@ -1,19 +1,49 @@
 import Container from "./UI/Container";
-import { type Timer as TimerProps } from "../store/timers-context";
-import { useEffect, useState } from "react";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  /** useRef - will not get created when the component function runs again
+   * The value stored will stay
+   */
+  const interval = useRef<number | null>(null);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
 
   /**
    * setInterval only run once event after the component re-renders and is re-execute
    * useEffect is to prevent infinite loops
    */
   useEffect(() => {
-    setInterval(function () {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
-  }, []);
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (!isRunning && interval.current) {
+      clearInterval(interval.current);
+    }
+    return () => clearInterval(timer);
+
+    /**
+     * Added isRunning as dependency
+     * - when value of isRunning changed, effect function is executed
+     */
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
